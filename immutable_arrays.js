f1440a71a6d25529d323d98f04b531dfa097d6ab
Dein Name <dein@email.ch>
2026-02-14 11:27:51 +0100

    Aufgabe 2.3

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -9,3 +9,14 @@ console.log("Ursprüngliches Array:", items);
 // 2. Neues Array mit zusätzlichem Element
 const newItems = [...items, { id: 3, name: "Artikel C", price: 5.0 }];
 console.log("Array mit neuem Element:", newItems);
+
+// 3. Neues Array mit aktualisiertem Element
+// Also ich werde den Preis von "Artikel 1" (id: 1)
+const updatedItems = items.map(item => {
+    if (item.id === 1) {
+        // Neues Objekt wird zurückgeben statt das alte zu ändern
+        return { ...item, price: 15.0 };
+    }
+    return item;
+});
+console.log("Array mit aktualisiertem Preis:", updatedItems);
