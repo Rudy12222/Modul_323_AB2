2e23d6b57ad3d2964d4e24bb7d7b264c3f59c8ff
Dein Name <dein@email.ch>
2026-02-14 11:18:11 +0100

    Aufgabe 2 und 3

--- immutable_objects.js
+++ immutable_objects.js
@@ -2,3 +2,16 @@
 const id = 123;
 console.log("ID:", id);
 // id = 456; // Würde einen Fehler geben
+
+// 2. Konstante Variable mit Objekt
+const user = {
+    name: "Max Mustermann",
+    age: 25,
+    city: "Zürich"
+};
+console.log("Ursprüngliches Objekt:", user);
+
+// 3. Eigenschaft hinzufügen
+// also ich nutze einen Spread Operator um eine Kopie zu erstellen und die neue Eingenschaft anzuhägnen
+const userWithEmail = { ...user, email: "max@example.com" };
+console.log("Objekt mit neuer Eigenschaft:", userWithEmail);
