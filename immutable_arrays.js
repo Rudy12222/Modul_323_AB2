7ef8c9f644c627ca5197839c48b6c2bd76f624df
Dein Name <dein@email.ch>
2026-02-14 11:31:32 +0100

    Aufgabe 2.6

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -20,3 +20,37 @@ const updatedItems = items.map(item => {
     return item;
 });
 console.log("Array mit aktualisiertem Preis:", updatedItems);
+
+
+// 4. Element mit kleinstem Identifikator entfernen
+// Kleinste ID ist 1. Ich filtere alle Elemente die nicht die id 1 haben
+const filteredItems = items.filter(item => item.id !== 1);
+console.log("Array ohne Element mit kleinster ID:", filteredItems);
+
+
+
+// 5. Durchschnitt berechnen
+const reviews = [4.5, 4.0, 5.0, 2.0, 1.0, 5.0, 3.0, 4.0, 1.0, 5.0, 4.5, 3.0, 2.5, 2.0];
+
+const average = reviews.reduce((acc, val, idx, arr) => {
+    acc += val;
+    if (idx === arr.length - 1) {
+        return acc / arr.length;
+    }
+    return acc;
+}, 0);
+console.log("Durchschnitt der Bewertungen:", average.toFixed(2));
+
+
+// 6. Werte gruppieren und zählens
+const groupedReviews = reviews.reduce((counter, val) => {
+    let key = "";
+    if (val >= 4) key = "good";
+    else if (val >= 2.5) key = "ok";
+    else key = "bad";
+
+    counter[key] = (counter[key] || 0) + 1;
+    return counter;
+}, {});
+
+console.log("Gruppierte Bewertungen:", groupedReviews);
