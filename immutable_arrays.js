bd3d4b3f904bc67911a4e07a3d6e0962652bbfba
Dein Name <dein@email.ch>
2026-02-14 11:23:34 +0100

    Aufgabe 2.1

---
+++ immutable_arrays.js
@@ -0,0 +1,6 @@
+// 1. Array mit Objekten und eindeutigem Identifikator
+const items = [
+    { id: 1, name: "Artikel A", price: 10.5 },
+    { id: 2, name: "Artikel B", price: 20.0 }
+];
+console.log("Ursprüngliches Array:", items);
